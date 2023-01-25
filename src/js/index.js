752203fdef72f5d6416adaa8ccf923c04804e4d4
bitard335 <bitard335@gmail.com>
2023-01-25 12:00:59 +0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ import '../slider-orig/swiper-bundle.min.js'
 import '../slider-orig/swiper-bundle.min.css'
 
 import '../scss/style.scss'
-import '../blocks/page/page-main/page-service-block/read-more-button/read-more-button.js'
-import '../blocks/page/page-main/page-service/show-services-button/show-services-button.js'
-import '../blocks/page/page-main/page-service/swiper/swiper.js'
-import '../blocks/burger-button/burger-button.js'
-import '../blocks/call-button/call-button.js'
-import '../blocks/chat-button/chat-button.js'
-import '../auto-height/auto-height.js'
-import '../col-head-width/col-head-width.js'
+import './read-more-button/read-more-button.js'
+import './show-services-button/show-services-button.js'
+import './swiper/swiper.js'
+import './burger-button/burger-button.js'
+import './call-button/call-button.js'
+import './chat-button/chat-button.js'
+import './auto-height/auto-height.js'
+import './col-head-width/col-head-width.js'
 
 
 console.log('Index.js Works!')
